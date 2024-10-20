ac681e3ca8dadcb3061bbaaba733740dcfd3f71c
BudiLim <limmtwenc@gmail.com>
2024-10-21 03:31:19 +0700

    feat: navbar

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,6 @@
 export default function Home() {
   return (
     <main>
-      <div className="text-center text-2xl text-white">
-          <h1>MURID PADUKA ILHAM</h1>
-      </div>
     </main>
   )
 }
