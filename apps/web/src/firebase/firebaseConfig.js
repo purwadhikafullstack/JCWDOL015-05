7cb06c5dfbaa4a3ac496380310b0f26cb247fb65
unknown <daffafadila00@gmail.com>
2024-11-21 11:20:59 +0700

    fix: firebase config

--- apps/web/src/firebase/firebaseConfig.js
+++ apps/web/src/firebase/firebaseConfig.js
@@ -8,19 +8,22 @@ import {getAuth, getauth, GoogleAuthProvider, signInWithPopup, signInWithRedirec
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC7sCN3eNdoocTf0Jg_BiijGFWRoORRio4",
-  authDomain: "wash-now-706cf.firebaseapp.com",
-  projectId: "wash-now-706cf",
-  storageBucket: "wash-now-706cf.firebasestorage.app",
-  messagingSenderId: "998673475341",
-  appId: "1:998673475341:web:09e91871e94cda24223244",
-  measurementId: "G-2Y01Q05XVP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth  = getAuth(app)
 const provider = new GoogleAuthProvider()
-const analytics = getAnalytics(app);
+let analytics
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
 
 export {auth, provider, signInWithPopup, signInWithRedirect}
